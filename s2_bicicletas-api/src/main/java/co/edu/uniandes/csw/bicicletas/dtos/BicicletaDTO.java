9b126ac3347582baef7a540f80d0e042a2bd1bff
Andrea Bayona <ma.bayona@uniandes.edu.co>
2019-02-10 21:43:40 -0500

    DTOs Reseña y Bicicleta

--- s2_bicicletas-api/src/main/java/co/edu/uniandes/csw/bicicletas/dtos/BicicletaDTO.java
+++ s2_bicicletas-api/src/main/java/co/edu/uniandes/csw/bicicletas/dtos/BicicletaDTO.java
@@ -5,10 +5,121 @@
  */
 package co.edu.uniandes.csw.bicicletas.dtos;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Andrea
  */
-public class BicicletaDTO {
+public class BicicletaDTO implements Serializable {
+    
+    private String descripcion;
+    
+    private String referencia;
+    
+    private double precio;
+    
+    private boolean usada;
+    
+    private int stock;
+    
+    private String[] album;
+    
+    //private MarcaDTO marca;
+    
+    //private CategoriaDTO categoria;
+    
+    
+     /**
+     * Constructor por defecto
+     */
+    public BicicletaDTO(){
+        
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the referencia
+     */
+    public String getReferencia() {
+        return referencia;
+    }
+
+    /**
+     * @param referencia the referencia to set
+     */
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the usada
+     */
+    public boolean isUsada() {
+        return usada;
+    }
+
+    /**
+     * @param usada the usada to set
+     */
+    public void setUsada(boolean usada) {
+        this.usada = usada;
+    }
+
+    /**
+     * @return the stock
+     */
+    public int getStock() {
+        return stock;
+    }
+
+    /**
+     * @param stock the stock to set
+     */
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    /**
+     * @return the album
+     */
+    public String[] getAlbum() {
+        return album;
+    }
+
+    /**
+     * @param album the album to set
+     */
+    public void setAlbum(String[] album) {
+        this.album = album;
+    }
+       
     
 }
---
+++ s2_bicicletas-api/src/main/java/co/edu/uniandes/csw/bicicletas/dtos/BicicletaDetailDTO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.bicicletas.dtos;
+
+import java.util.List;
+
+/**
+ *
+ * @author Andrea
+ */
+public class BicicletaDetailDTO {
+    
+    private List<ResenaDTO> resenas;
+    
+    
+    public BicicletaDetailDTO(){
+        
+    }
+
+    /**
+     * @return the resenas
+     */
+    public List<ResenaDTO> getResenas() {
+        return resenas;
+    }
+
+    /**
+     * @param resenas the resenas to set
+     */
+    public void setResenas(List<ResenaDTO> resenas) {
+        this.resenas = resenas;
+    }
+    
+}
