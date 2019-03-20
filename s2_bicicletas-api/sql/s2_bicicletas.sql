b5c421b83d604d4e88870a333532c5f5f298679c
Andres Donoso <af.donoso@uniandes.edu.co>
2019-03-19 21:04:02 -0500

    Modificado

--- s2_bicicletas-api/sql/s2_bicicletas.sql
+++ s2_bicicletas-api/sql/s2_bicicletas.sql
@@ -6,5 +6,3 @@ DELETE FROM MedioPagoEntity;
 INSERT INTO CategoriaEntity (id, nombre) VALUES (100, 'BMX');
 INSERT INTO CategoriaEntity (id, nombre) VALUES (200, 'Estatica');
 
-INSERT INTO CompradorEntity ();
-
