7ca063a894b227c81fe77586e4d55238279b2c2d
Andres Donoso <af.donoso@uniandes.edu.co>
2019-04-12 18:35:34 -0500

    Modificado

--- s2_bicicletas-api/sql/s2_bicicletas.sql
+++ s2_bicicletas-api/sql/s2_bicicletas.sql
@@ -4,3 +4,13 @@ DELETE FROM CompradorEntity;
 DELETE FROM MedioPagoEntity;
 delete from VendedorEntity;
 
+INSERT INTO CategoriaEntity(nombre) VALUES('Urbana');
+INSERT INTO CategoriaEntity(nombre) VALUES('Estatica');
+INSERT INTO CategoriaEntity(nombre) VALUES('BMX');
+INSERT INTO CategoriaEntity(nombre) VALUES('Montaña');
+INSERT INTO CategoriaEntity(nombre) VALUES('Deportiva');
+INSERT INTO CategoriaEntity(nombre) VALUES('Todoterreno');
+
+insert into BicicletaEntity (id, calificacion, descripcion, referencia, precio, usada, stock, album, marca_id, categoria_id)
+
+values (100, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'XYZ-123', 800000, 0, 100, 'a', 100,100);
